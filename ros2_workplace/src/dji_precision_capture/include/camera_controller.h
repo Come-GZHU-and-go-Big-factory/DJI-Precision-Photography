7acbfd3e49caa30ab9791561486c088ba44105f1
LZP <2411957706@qq.com>
2026-01-04 20:18:40 +0800

    ros2 work commit

---
+++ ros2_workplace/src/dji_precision_capture/include/camera_controller.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_MANAGER_H
+#define CAMERA_MANAGER_H
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+// // INCLUDE 
+// #include "camera_manager.h"
+// #include "gimbal_manager.h"
+// #include "rcl"
+// class camera_controller()
+// {
+//     public:
+
+//     private:
+//     void dji_control_loop();
+
+// };
+
+// void camera_controller::dji_control_loop()
+// {
+
+// }
+#endif
